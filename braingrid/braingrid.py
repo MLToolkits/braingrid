0ae3c059a963a1fddb977c93489a51862b65f2cf
kenny <97374837+kankenny@users.noreply.github.com>
2025-04-23 23:06:35 +0000

    chore: smiley

---
+++ braingrid/braingrid.py
@@ -0,0 +1,11 @@
+def hello_world():
+    print("Hello World!")
+
+
+def brain_grid():
+    print(f"{'-'*40}>{':)'.center(40)}<{'-'*40}")
+
+
+if __name__ == "__main__":
+    hello_world()
+    brain_grid()
